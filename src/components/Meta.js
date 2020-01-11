9c0e7ba6da94a8d808492a25ba6c08137670ed78
yonicb <yonicb20@gmail.com>
2020-01-11 18:27:58 -0500

    refactor: 💡 Add SEO features

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -1,16 +1,16 @@
 import Head from 'next/head'
 
 export default function Meta(props) {
-   return (
-      <>
-         <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta charSet="utf-8" />
-            <title>{props.siteTitle}</title>
-            <meta name="Description" content={props.description}></meta>
-         </Head>
-         <style jsx global>
-            {`
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta charSet="utf-8" />
+                <title>{props.siteTitle}</title>
+                <meta name="Description" content={props.description}></meta>
+            </Head>
+            <style jsx global>
+                {`
                     @import url("https://fonts.googleapis.com/css?family=Work+Sans&display=swap");
 
                     * {
@@ -222,7 +222,7 @@ export default function Meta(props) {
                         }
                     }
                 `}
-         </style>
-      </>
-   )
+            </style>
+        </>
+    )
 }
