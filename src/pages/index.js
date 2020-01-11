8148592c32f248dc936c88ec65a1965b0d7ff18a
yonicb <yonicb20@gmail.com>
2020-01-11 18:29:41 -0500

    refactor: 💡 Add all post functions added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
+import matter from 'gray-matter'
+
 import Layout from "../components/Layout";
 import BlogList from "../components/BlogList";
 
 const Index = (props) => {
   return (
-    <Layout 
-      pathname="/" 
-      siteTitle={props.title} 
-      siteDescription={props.description}
-    >
+    <Layout pathname="/" siteTitle={props.title} siteDescription={props.description}>
       <section>
-        <BlogList />
+        <BlogList allBlogs={props.allBlogs}/>
       </section>
     </Layout>
   );
@@ -18,8 +16,31 @@ const Index = (props) => {
 export default Index;
 
 Index.getInitialProps = async function() {
-  const configData = await import(`../data/config.json`)
+  const siteConfig = await import(`../data/config.json`)
+   //get posts & context from folder
+   const posts = (context => {
+    const keys = context.keys();
+    const values = keys.map(context);
+    const data = keys.map((key, index) => {
+      // Create slug from filename
+      const slug = key
+        .replace(/^.*[\\\/]/, "")
+        .split(".")
+        .slice(0, -1)
+        .join(".");
+      const value = values[index];
+      // Parse yaml metadata & markdownbody in document
+      const document = matter(value.default);
+      return {
+        document,
+        slug
+      };
+    });
+    return data;
+  })(require.context("../posts", true, /\.md$/));
+
   return {
-    ...configData
+    allBlogs: posts,
+    ...siteConfig,
   }
 }
